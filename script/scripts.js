a5cecdee249c854d6429559d56a7fe161a03ba8e
kitamreik <kitdamreik@gmail.com>
2024-02-06 22:07:29 -0700

    first commit

---
+++ script/scripts.js
@@ -0,0 +1,31 @@
+//alert('is this thing on?!');
+
+// define variable that store a "house", a "cat" and the days of the week (as a number). 
+// var, let, const
+
+const week = 7;
+let pet = "cat";
+let home = "house";
+
+console.log(week, pet, home);
+
+//Ask the user what their fav color is and store it. 
+// let answer = prompt("Hey Kit, focus on studying more coding silly")
+
+//console.log(answer) //Then print out their answer and show them.
+
+// Tell Kit to keep studying coding and print out the answer
+// alert("Hey, Kit keep studying coding!!!")
+// console.log(alert("Hey, Kit keep studying coding!!!"))
+ //let videogame = answer;
+// let videogame = prompt("Hey what is your fav video game")
+
+// const study = "Hey, Kit - study coding";
+// console.log(study);
+let foo = prompt("Hey what is your fav video game");
+let answer = prompt("Hey Kit, focus on studying more coding silly");
+alert(foo)
+//console.log(answer)
+//alert(videogame) //undefined --> STOP
+
+console.log(answer) //unreachable if videogame is undefined and uncommented out
